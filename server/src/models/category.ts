4c884b3e188a045e299c2a93a7cdb7b39fec3336
taesung1993 <cheonyulin@gmail.com>
2023-05-24 01:34:05 +0900

    add class model

--- server/src/models/category.ts
+++ server/src/models/category.ts
@@ -1,9 +1,15 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../config/db";
+import { Class } from "./class";
 
 export interface ICategory {
   id: number;
   name: string;
+}
+
+interface Models {
+  Category: typeof Category;
+  Class: typeof Class;
 }
 
 type CategoryCreationAttributes = Omit<ICategory, "id">;
@@ -11,6 +17,8 @@ type CategoryCreationAttributes = Omit<ICategory, "id">;
 export class Category extends Model<ICategory, CategoryCreationAttributes> {
   declare id: number;
   declare name: string;
+
+  declare static associate: (models: Models) => void;
 }
 
 Category.init(
@@ -31,5 +39,12 @@ Category.init(
     sequelize,
   }
 );
+
+Category.associate = (models) => {
+  Category.hasMany(models.Class, {
+    foreignKey: "categoryId",
+    sourceKey: "id",
+  });
+};
 
 export default {};
---
+++ server/src/models/class.ts
@@ -0,0 +1,86 @@
+import { Association, DataTypes, Model } from "sequelize";
+import { sequelize } from "../config/db";
+import { Category } from "./category";
+
+export interface IClass {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  startDate: string;
+  status: string;
+  maximumPerson: number;
+  categoryId: number;
+}
+
+interface Models {
+  Category: typeof Category;
+  Class: typeof Class;
+}
+
+type CategoryCreationAttributes = Omit<IClass, "id">;
+
+export class Class extends Model<IClass, CategoryCreationAttributes> {
+  declare id: number;
+  declare name: string;
+  declare description: string;
+  declare location: string;
+  declare startDate: string;
+  declare status: string;
+  declare maximumPerson: number;
+  declare categoryId: number;
+
+  declare static associate: (models: Models) => void;
+}
+
+Class.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    startDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    maximumPerson: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    categoryId: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "class",
+    sequelize,
+  }
+);
+
+Class.associate = (models) => {
+  Class.belongsTo(models.Category, {
+    foreignKey: "categoryId",
+    targetKey: "id",
+  });
+};
+
+export default {};
