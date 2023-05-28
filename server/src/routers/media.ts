e3a38d25714f842daed73e9421ec13e3212910dc
taesung1993 <cheonyulin@gmail.com>
2023-05-28 11:15:48 +0900

    add error case in post://media

--- server/src/routers/media.ts
+++ server/src/routers/media.ts
@@ -1,12 +1,49 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const ERRORS = {
+  NOT_ALLOW_MIME_TYPE: "NOT_ALLOW_MIME_TYPE",
+} as const;
+
+const upload = multer({
+  dest: "uploads/",
+  fileFilter: function (req, file, cb) {
+    const { mimetype } = file;
+    const supportedMimetypes = ["image/png", "image/jpg", "image/jpeg"];
+
+    if (supportedMimetypes.includes(mimetype)) {
+      cb(null, true);
+      return;
+    }
+
+    cb(new Error("NOT_ALLOW_MIME_TYPE"));
+  },
+}).single("file");
+
 const router = express.Router();
 
-router.post("/", upload.single("image"), (req: Request, res: Response) => {
-  console.log(req);
-  res.json({ message: "hello" });
+router.post("/", (req: Request, res: Response) => {
+  upload(req, res, function (error) {
+    if (error) {
+      if (error instanceof multer.MulterError) {
+        console.log(error);
+        return res.json({ message: "알 수 없는 에러가 발생하였습니다." });
+      }
+
+      switch (error?.message) {
+        case ERRORS.NOT_ALLOW_MIME_TYPE:
+          return res.status(415).json({
+            message: ".png, .jpg, .jpeg 형식의 파일만 업로드할 수 있습니다.",
+          });
+        default:
+          return res
+            .status(500)
+            .json({ message: "알 수 없는 에러가 발생하였습니다." });
+      }
+    }
+
+    res.json({ message: "성공 하였습니다." });
+  });
 });
 
 export default router;
