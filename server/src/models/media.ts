e34d976e4e6499ec9b82413122a402d18e4f1ab3
taesung1993 <cheonyulin@gmail.com>
2023-05-30 02:12:13 +0900

    add media model

---
+++ server/src/models/media.ts
@@ -0,0 +1,51 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  Sequelize,
+  NonAttribute,
+} from "sequelize";
+import { sequelize } from "../config/db";
+
+export type MediaModel = "class" | "user";
+
+export interface IMedia {
+  id: string;
+  url: string;
+  model: MediaModel;
+}
+
+export class Media extends Model<
+  InferAttributes<Media>,
+  InferCreationAttributes<Media>
+> {
+  declare id: CreationOptional<string>;
+  declare model: MediaModel;
+  declare url: CreationOptional<string>;
+}
+
+Media.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "media",
+    sequelize,
+  }
+);
+
+export default {};
