5a2fb52184320578b0a32c11fe43f961c8ad729e
taesung1993 <cheonyulin@gmail.com>
2023-05-30 02:11:43 +0900

    change multer as middleware

---
+++ server/src/middlewares/multer.middleware.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from "express";
+import multer from "multer";
+
+const ERRORS = {
+  NOT_ALLOW_MIME_TYPE: "NOT_ALLOW_MIME_TYPE",
+} as const;
+
+export const mediaMulter = multer({
+  storage: multer.memoryStorage(),
+  dest: "uploads/",
+  fileFilter: function (req, file, cb) {
+    const { mimetype } = file;
+
+    if (mimetype.match(/image\/(jpg|jpeg|png)$/)) {
+      cb(null, true);
+      return;
+    }
+
+    cb(new Error("NOT_ALLOW_MIME_TYPE"));
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+export const multipleMediaMulter = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const uploadFiles = mediaMulter.array("files", 10);
+
+  uploadFiles(req, res, async function (error) {
+    if (error) {
+      if (error instanceof multer.MulterError) {
+        console.log(error);
+        return res.json({ message: "알 수 없는 에러가 발생하였습니다." });
+      }
+
+      switch (error?.message) {
+        case ERRORS.NOT_ALLOW_MIME_TYPE:
+          return res.status(415).json({
+            message: ".png, .jpg, .jpeg 형식의 파일만 업로드할 수 있습니다.",
+          });
+        default:
+          return res
+            .status(500)
+            .json({ message: "알 수 없는 에러가 발생하였습니다." });
+      }
+    }
+
+    next();
+  });
+};
