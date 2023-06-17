e9537ae70ba44d03cdc6db827f35bd095a95fbab
taesung1993 <cheonyulin@gmail.com>
2023-06-17 17:36:39 +0900

    add auth-middleware

---
+++ server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import CustomError from "@classes/custom-error.class";
+import { User } from "@models/user";
+import jwtService from "@services/jwt.service";
+import { NextFunction, Request, Response } from "express";
+import { JsonWebTokenError } from "jsonwebtoken";
+import { BaseError } from "sequelize";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userIdFromToken = jwtService.getUserIdFromRequest(req);
+    const account = await User.findByPk(userIdFromToken);
+    const { password: _, ...user } = account!.dataValues;
+
+    res.locals.user = user;
+
+    next();
+  } catch (error) {
+    if (error instanceof CustomError) {
+      return res.status(error.status).json({ error: error.message });
+    }
+
+    if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({ error: error.message });
+    }
+
+    if (error instanceof BaseError) {
+      return res.status(401).json({ error: error.message });
+    }
+
+    res.status(401).json({ error: "유효한 자격 증명이 없습니다." });
+  }
+};
