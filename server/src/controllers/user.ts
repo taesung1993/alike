8435e2b7cbe72c4bf69184605b3793f7d41860c6
taesung1993 <cheonyulin@gmail.com>
2023-07-04 13:30:24 +0900

    add and apply user-controller

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { User } from "@models/user";
+import jwtService from "@services/jwt.service";
+import { BaseError } from "sequelize";
+
+export const signUpNewUser = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const passwordWithHash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    password: passwordWithHash,
+    email,
+  });
+
+  const token = jwtService.createJWT(user);
+
+  return res.status(201).json({
+    token,
+  });
+};
+
+export const signInCurrentUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (user) {
+    const isSamePassword = await bcrypt.compare(password, user.password);
+
+    if (isSamePassword) {
+      const token = jwtService.createJWT(user);
+
+      return res.status(201).json({
+        token,
+      });
+    }
+  }
+
+  return res.status(401).json({ error: "Authentication failed" });
+};
+
+export const getMe = async (_: Request, res: Response) => {
+  const user = res.locals.user;
+
+  delete res.locals["user"];
+
+  return res.status(200).json(user);
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findByPk(id);
+
+    user?.destroy({ force: true });
+
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    if (error instanceof BaseError) {
+      return res.status(404).json({ message: error.message });
+    }
+
+    return res
+      .status(500)
+      .json({ message: "알 수 없는 에러가 발생하였습니다." });
+  }
+};
--- server/src/routers/user.ts
+++ server/src/routers/user.ts
@@ -1,79 +1,19 @@
-import express, { Request, Response } from "express";
-import bcrypt from "bcrypt";
-import { User } from "@models/user";
-import jwtService from "@services/jwt.service";
+import express from "express";
 import { authMiddleware } from "@middlewares/auth.middleware";
-import { BaseError } from "sequelize";
+import {
+  deleteUser,
+  getMe,
+  signInCurrentUser,
+  signUpNewUser,
+} from "@controllers/user";
 
 const router = express.Router();
 
-router.post("/sign-up", async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  const passwordWithHash = await bcrypt.hash(password, 10);
+router.post("/sign-up", signUpNewUser);
+router.post("/sign-in", signInCurrentUser);
 
-  const user = await User.create({
-    name,
-    password: passwordWithHash,
-    email,
-  });
+router.get("/me", authMiddleware, getMe);
 
-  if (user) {
-    const token = jwtService.createJWT(user);
-
-    return res.status(201).json({
-      token,
-    });
-  }
-});
-
-router.post("/sign-in", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({
-    where: {
-      email,
-    },
-  });
-
-  if (user) {
-    const isSamePassword = await bcrypt.compare(password, user.password);
-
-    if (isSamePassword) {
-      const token = jwtService.createJWT(user);
-
-      return res.status(201).json({
-        token,
-      });
-    }
-  }
-
-  return res.status(401).json({ error: "Authentication failed" });
-});
-
-router.get("/me", authMiddleware, async (req: Request, res: Response) => {
-  const user = res.locals.user;
-
-  delete res.locals["user"];
-
-  return res.status(200).json(user);
-});
-
-router.delete("/:id", authMiddleware, async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    const user = await User.findByPk(id);
-
-    user?.destroy({ force: true });
-
-    return res.status(200).json({ message: "success" });
-  } catch (error) {
-    if (error instanceof BaseError) {
-      return res.status(404).json({ message: error.message });
-    }
-
-    return res
-      .status(500)
-      .json({ message: "알 수 없는 에러가 발생하였습니다." });
-  }
-});
+router.delete("/:id", authMiddleware, deleteUser);
 
 export default router;
