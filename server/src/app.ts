e91d8e3323b8848ee899e5e064c5fff4cfc84b1e
taesung1993 <cheonyulin@gmail.com>
2023-07-22 21:12:22 +0900

    initialize swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,13 +3,45 @@ import helmet from "helmet";
 import routers from "@routers/.";
 import { sequelize } from "@config/db";
 import { redis } from "@config/redis";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import "@models/assocations";
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
 const app: Express = express();
+
 export const initServer = async (port: number) => {
   try {
     await sequelize.sync({ force: false });
     await redis.connect();
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
     app.listen(port, () => {
       console.log(`[server]: Server is running at ${port}`);
     });
