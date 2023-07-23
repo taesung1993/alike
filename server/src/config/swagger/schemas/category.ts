2c45eaaf24245c51a0eb162980df7fee9b7f360d
taesung1993 <cheonyulin@gmail.com>
2023-07-23 17:46:11 +0900

    add all properties in category schema

--- server/src/config/swagger/schemas/category.ts
+++ server/src/config/swagger/schemas/category.ts
@@ -5,6 +5,22 @@ const category = {
       type: "integer",
       description: "카테고리 아이디",
     },
+    name: {
+      type: "string",
+      description: "카테고리 이름",
+    },
+    createdAt: {
+      type: "string",
+      description: "생성 날짜",
+    },
+    updatedAt: {
+      type: "string",
+      description: "수정 날짜",
+    },
+    classCount: {
+      type: "integer",
+      description: "카테고리에 소속된 클래스 개수",
+    },
   },
 };
 
